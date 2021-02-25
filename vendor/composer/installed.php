cb597a65cf572d38740065ea92042b5533904f9f
somchay <55747347+somchaynoy@users.noreply.github.com>
2021-02-25 09:41:02 +0700

    Initial

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b0dd1f01268d0cabe96032bdc5f912f8720a2f33',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b0dd1f01268d0cabe96032bdc5f912f8720a2f33',
+    ),
+    'linecorp/line-bot-sdk' => 
+    array (
+      'pretty_version' => '6.2.0',
+      'version' => '6.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9d9a0d889c4a83b4521327882bf60be38b476e3',
+    ),
+  ),
+);
