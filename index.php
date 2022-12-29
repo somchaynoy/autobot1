005912c07ea1980aa59199af3e105553274ca0bf
somchay <55747347+somchaynoy@users.noreply.github.com>
2022-12-29 20:40:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
 
 //Token
-$channel_token='r5GTCeVRSWqJyCtiT8YHE9n4I1+BYRSh4XXEJn7DlrDC+2MZySV8m4Thsz9NOZlh0wq2E4IhN3Hl/yhqWS4rDAIX7/Q6oxzUYhMPO5aqGkDbLhyyvY0l4RkwSblBzQVtD2+8uJotM8AnhUzsVeSXNgdB04t89/1O/w1cDnyilFU=;
+$channel_token='r5GTCeVRSWqJyCtiT8YHE9n4I1+BYRSh4XXEJn7DlrDC+2MZySV8m4Thsz9NOZlh0wq2E4IhN3Hl/yhqWS4rDAIX7/Q6oxzUYhMPO5aqGkDbLhyyvY0l4RkwSblBzQVtD2+8uJotM8AnhUzsVeSXNgdB04t89/1O/w1cDnyilFU=';
 
 $channel_secret='7892716414d782ce55cb0076ed4d536b';
 
